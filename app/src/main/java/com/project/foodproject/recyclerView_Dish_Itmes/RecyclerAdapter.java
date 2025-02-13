7fc542d48fe65345f3bfc1005a21d6cf58a20454
junho <junho5508@naver.com>
2025-02-14 00:56:39 +0900

    체크박스 상태에 따른 roomdb에 요리명 추가하는 메서드 및 제거하는 메서드 작업

--- app/src/main/java/com/project/foodproject/recyclerView_Dish_Itmes/RecyclerAdapter.java
+++ app/src/main/java/com/project/foodproject/recyclerView_Dish_Itmes/RecyclerAdapter.java
@@ -1,21 +1,30 @@
 package com.project.foodproject.recyclerView_Dish_Itmes;
 
+import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.project.foodproject.R;
 import com.project.foodproject.RecipeActivity;
+import com.project.foodproject.favoriteRecipesRoomDB.FavoriteDataModel;
+import com.project.foodproject.favoriteRecipesRoomDB.RecipeDB;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Viewholder> {
     private ArrayList<RecyclerDataModel> dataModels;
@@ -42,11 +51,15 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Viewho
         ArrayList<String> ManualList = dataModels.get(position).getManualList();
         ArrayList<String> ManualImgList = dataModels.get(position).getManualImgList();
 
+        FavoriteDataModel dataModel = new FavoriteDataModel();
+        RecipeDB recipeDB = RecipeDB.getInstance(holder.itemView.getContext());
+
 
         holder.tv_foodName.setText(foodName);
         Glide.with(holder.itemView.getContext())
                 .load(imgUrl)
                 .into(holder.img_foodImage);
+
 
 
         holder.btn_showRecipe.setOnClickListener(new View.OnClickListener() {
@@ -61,6 +74,89 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Viewho
                 holder.itemView.getContext().startActivity(intent);
             }
         });
+
+
+        checkIfFavorite(dataModel, recipeDB, foodName, holder.itemView.getContext(), holder.CB_favorites);
+
+
+        holder.CB_favorites.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b == true) {
+                    handleCheckBoxChecked(dataModel, recipeDB, foodName, holder.itemView.getContext());
+
+                } else if (b == false) {
+                    handleCheckBoxUnchecked(recipeDB, foodName, holder.itemView.getContext());
+                }
+            }
+        });
+
+    }
+
+
+    // 해당 레시피가 이미 즐겨찾기에 추가되어 있는지 확인하는 메서드
+    private void checkIfFavorite(FavoriteDataModel dataModel, RecipeDB data,
+                                       String nickname, Context context, CheckBox checkBox){
+        dataModel.setDishName(nickname);
+        new Thread(() -> {
+            if (data.recipeDao().isRecipeExists(dataModel.getDishName()) > 0) {
+                ((AppCompatActivity) context).runOnUiThread(() ->
+                        checkBox.setChecked(true)
+                );
+            } else {
+                ((AppCompatActivity) context).runOnUiThread(() ->
+                        checkBox.setChecked(false)
+                );
+            }
+
+            List<FavoriteDataModel> allRecipe = data.recipeDao().getAllDataSortedByDishName();
+            for (FavoriteDataModel Recipe : allRecipe) {
+                Log.d("DB_Check", "dishName : "+Recipe.getDishName() );
+            }
+
+        }).start();
+
+    }
+
+
+    //roomDB에 즐겨찾기 레시피추가
+    private void handleCheckBoxChecked(FavoriteDataModel dataModel, RecipeDB data,
+                                       String nickname, Context context) {
+        dataModel.setDishName(nickname);
+        Log.d("PlayerDataModel", "Nickname: " + dataModel.getDishName());
+
+        new Thread(() -> {
+            if (data.recipeDao().isRecipeExists(dataModel.getDishName()) == 0) {
+                data.recipeDao().setInsertFavoriteRecipe(dataModel);
+            }
+
+            List<FavoriteDataModel> allRecipe = data.recipeDao().getAllDataSortedByDishName();
+            for (FavoriteDataModel Recipe : allRecipe) {
+                Log.d("DB_Check", "dishName : "+Recipe.getDishName() );
+            }
+
+        }).start();
+
+    }
+
+    //roomDB에 즐겨찾기 레시피 삭제
+    private void handleCheckBoxUnchecked(RecipeDB recipeDB, String dishName, Context context) {
+        new Thread(() -> {
+            try {
+                recipeDB.recipeDao().deleteRecipeByDishName(dishName);
+
+                List<FavoriteDataModel> allRecipe = recipeDB.recipeDao().getAllDataSortedByDishName();
+                for (FavoriteDataModel recipe : allRecipe) {
+                    Log.d("DB_Check", "dishName: " +recipe.getDishName());
+                }
+                ((AppCompatActivity) context).runOnUiThread(() ->
+                        Toast.makeText(context, dishName + " 삭제되었습니다", Toast.LENGTH_SHORT).show()
+                );
+            } catch (Exception e) {
+                Log.e("checked Room Data", "no data at room");
+            }
+        }).start();
+
     }
 
     @Override
@@ -72,12 +168,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.Viewho
         TextView tv_foodName;
         ImageView img_foodImage;
         Button btn_showRecipe;
+        CheckBox CB_favorites;
 
         public Viewholder(@NonNull View itemView) {
             super(itemView);
             tv_foodName = itemView.findViewById(R.id.tv_foodName);
             img_foodImage = itemView.findViewById(R.id.img_foodImage);
             btn_showRecipe = itemView.findViewById(R.id.btn_showRecipe);
+            CB_favorites = itemView.findViewById(R.id.CB_favorites);
         }
     }
 }
